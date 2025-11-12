90f727b907fbf62c0667ef55b1bc4d1875ad1a07
Johnny Mast <mastjohnny@gmail.com>
2025-11-12 22:36:43 +0100

    Remove redundant whitespace in Dances module

--- README.md
+++ README.md
@@ -1,11 +1,27 @@
-<h1 align="center">SparkleBeat Game Interaction Code</h1>
-
 <p align="center">
     <img src="assets/sparklebeat.png" width="250"  height="250" alt="Picture of DJ SparkleBeat" />
 </p>
 
+# SparkleBeat Game Interaction Code
+
+LibGame allows SparkleBeat to interact with the 3DXChat game world. You are free to use it for your self, it has a pretty easy api to use.
+The current version is NOT a library yet but this will soon change once all the basic bugs are fixed (see [issues](https://github.com/silentboy3dx/LibGame/issues))
+
+```C++
+#include "LibGame/LibGame.hpp"
+
+using namespace LibGame::Module;
+
+int main() {
+
+    auto &interactions = LibGame::Interactions::GetInstance();
+    interactions.GetInteraction<Dances>().Dance(12);
+
+    return 0;
+}
+```
 # LibGame
-LibGame allows SparkleBeat to interact with the 3DXChat ganme world
+
 
 ## Asset path
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,68 +1,11 @@
 #include "LibGame/LibGame.hpp"
-#include "LibGame/io/Clipboard.hpp"
 
-#include "LibGame/action/primary/SlowDanceStatus.hpp"
-
-#include <thread>
-#include <chrono>
-
-#include "LibGame/action/primary/DanceStatus.hpp"
-#include "LibGame/action/primary/IdlePrimaryStatus.hpp"
-#include "LibGame/action/secondary/MovementSpeedSecondaryStatus.hpp"
-#include "LibGame/action/secondary/NakedSecondaryStatus.hpp"
-#include "LibGame/action/secondary/StraponSecondaryStatus.hpp"
-
-using namespace LibGame;
-using namespace LibGame::Action::Primary;
-using namespace LibGame::Action::Secondary;
 using namespace LibGame::Module;
-using namespace LibGame::Io;
 
 int main() {
 
-    auto &interactions = Interactions::GetInstance();
-    auto &moderation = interactions.GetInteraction<Moderation>();
-
-    // std::this_thread::sleep_for(std::chrono::milliseconds(3000));
-    //
-    // interactions.GetInteraction<Clothing>().UnDressAll();
-    //
-    // std::this_thread::sleep_for(std::chrono::milliseconds(2000));
-    //
-    // interactions.GetInteraction<Clothing>().DressAll();
-    // auto ctx = interactions.GetInteraction<Context>();
-
-    //
-    // // Primaire actie
-    // ctx.SetPrimaryAction<SlowDanceStatus>(SlowDanceStatus::Type::Face2Face, "Alice");
-    //
-    // // Secundaire actie
-    // ctx.AddSecondaryAction<EatStatus>(EatStatus::Type::Pizza);
-    //
-    // std::cout << ctx;
-
-    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
-    // interactions.GetInteraction<Clothing>().DressAll();
-
-    interactions.GetInteraction<Context>().before("=== Start ===")
-       // .SetPrimaryAction<IdlePrimaryStatus>(IdlePrimaryStatus::Type::Idle3)
-       // .SetPrimaryAction<DanceStatus>(DanceStatus::Type::Dance7)
-       // .AddSecondaryAction<EatStatus>(EatStatus::Type::Pizza)
-       // .AddSecondaryAction<MovementSpeedSecondaryStatus>(MovementSpeedSecondaryStatus::Type::Running)
-       // .AddSecondaryAction<StraponSecondaryStatus>(StraponSecondaryStatus::Type::Penis)
-       // .AddSecondaryAction<NakedSecondaryStatus>(NakedSecondaryStatus::Type::Undressed)
-       .add("Mood", "Happy")
-       .after("=== End ===");
-
-
-    // interactions.GetInteraction<Strapon>().Penis();
-    // interactions.GetInteraction<Strapon>().Toy1();
-    // interactions.GetInteraction<Strapon>().Toy2();
-    interactions.GetInteraction<Movement>().Run();
-
-    // ctx.SetPrimaryAction<IdlePrimaryStatus>(IdlePrimaryStatus::Type::Idle4);
-
-    std::cout << interactions.GetInteraction<Context>() << "\n";
+    auto &interactions = LibGame::Interactions::GetInstance();
+    interactions.GetInteraction<Dances>().Dance(12);
 
     return 0;
 }
