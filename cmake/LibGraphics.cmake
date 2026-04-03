14df47928d9c0aad153cb273e43c5eb59e942f26
Johnny Mast <mastjohnny@gmail.com>
2026-04-03 16:52:45 +0200

    Fetch and link `LibGraphics` via `FetchContent` in CMake to streamline dependency management.

--- cmake/LibGraphics.cmake
+++ cmake/LibGraphics.cmake
@@ -13,3 +13,18 @@ target_link_libraries(LibGame
         PUBLIC
         LibGraphics
 )
+include(FetchContent)
+
+# Fetch LibGraphics
+FetchContent_Declare(LibGraphics
+        GIT_REPOSITORY https://github.com/johnnymast/LibGraphics.git
+        GIT_TAG master
+)
+
+FetchContent_MakeAvailable(LibGraphics)
+
+# Link LibGraphics to LibGame
+target_link_libraries(LibGame
+        PUBLIC
+        LibGraphics
+)
