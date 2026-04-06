8cad84eac9b3161ad30c816d74d15c984c3aba0f
Johnny Mast <mastjohnny@gmail.com>
2026-04-06 11:04:04 +0200

    Refactor LibOS integration in CMake: replace `FetchContent` with `find_package` and adjust linking to use `LibOS::LibOS`.

--- cmake/LibOS.cmake
+++ cmake/LibOS.cmake
@@ -1,17 +1,3 @@
-include(FetchContent)
+find_package(LibOS REQUIRED)
 
-FetchContent_Declare(
-        LibOS
-        GIT_REPOSITORY https://github.com/silentboy3dx/LibOS.git
-        GIT_TAG        master
-)
-
-FetchContent_MakeAvailable(LibOS)
-
-target_include_directories(${PROJECT_NAME} PUBLIC
-        ${libos_SOURCE_DIR}/include
-)
-
-message(STATUS "Linking LibOS using includes ${libos_SOURCE_DIR}/include")
-
-target_link_libraries(${PROJECT_NAME} PRIVATE LibOS)
+target_link_libraries(LibGame PRIVATE LibOS::LibOS)
