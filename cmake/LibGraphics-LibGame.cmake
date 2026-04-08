8444fe2e2493a18d31c656e24ddbbb6a81723e0d
Silentboy <mastjohnny@gmail.com>
2026-04-08 23:13:27 +0200

    Update CMake configuration to include `LibGraphics` for Windows and streamline library linking.

---
+++ cmake/LibGraphics-LibGame.cmake
@@ -0,0 +1,5 @@
+if (WIN32)
+    list(APPEND CMAKE_PREFIX_PATH "C:/Program Files/LibGraphics")
+endif()
+
+find_package(LibGraphics REQUIRED) # FindUnreadMessages.cpp
--- cmake/LibScreenshots.cmake
+++ cmake/LibScreenshots.cmake
@@ -1,7 +1,12 @@
 if (WIN32)
+    list(APPEND CMAKE_PREFIX_PATH "C:/Program Files/LibGraphics")
+endif()
+
+find_package(LibGraphics REQUIRED)
+
+if (WIN32)
     list(APPEND CMAKE_PREFIX_PATH "C:/Program Files/LibScreenshots")
 endif()
 
-#find_package(LibGraphics REQUIRED)
 find_package(LibScreenshots REQUIRED)
 
