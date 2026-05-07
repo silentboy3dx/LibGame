757758bc41dd101a438be7986d161b9720dbd98f
Johnny Mast <mastjohnny@gmail.com>
2026-05-07 02:01:56 +0200

    Refactor asset loading: replace `std::cerr` with `Reporter`, adjust includes and namespace usage

--- src/asset/Assets.cpp
+++ src/asset/Assets.cpp
@@ -1,15 +1,19 @@
+#include "LibGame/asset/Assets.hpp"
+#include "LibGame/asset/AssetCache.hpp"
+#include "LibGame/LibGame.hpp"
+
 #include <LibGraphics/LibGraphics.hpp>
 
 #include <filesystem>
 #include <string>
 
-#include "LibGame/asset/Assets.hpp"
-#include "LibGame/asset/AssetCache.hpp"
-#include "LibGame/LibGame.hpp"
+#include "../../../LibCore/include/LibCore/report/Reporter.hpp"
 
 
 using LibGame::Exceptions::AssetException;
-using namespace LibGraphics;
+// using namespace LibGraphics;
+using LibCore::Report::Reporter;
+using LibCore::Report::arg;
 
 namespace LibGame::Asset {
 
@@ -17,8 +21,10 @@ namespace LibGame::Asset {
         auto& cache = AssetCache::Instance();
 
         if (!cache.Has(asset)) {
-            cache.PrintCache();
-            std::cerr << "Asset not preloaded: " << asset << std::endl;
+            // cache.PrintCache();
+            Reporter::Verbose("Asset not preloaded: {asset}",
+                typeid(Assets).name(),
+                arg("asset", asset));
         }
 
         auto imgPtr = cache.Load(asset);
